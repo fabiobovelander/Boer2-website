f4e9fe411be3e89799d922c09fdc49259061d636
fabiobovelander <fabiobovelander@gmail.com>
2026-02-04 11:07:52 +0100

    Remove ChatAssistant import from App.tsx
    Removed import statement for ChatAssistant component.

--- Boer2 website/App.tsx
+++ Boer2 website/App.tsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import ChatAssistant from './components/ChatAssistant';
 
 const App: React.FC = () => {
   return (
